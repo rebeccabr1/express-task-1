469201748377152ad04ce9a881b44d920a11ef5d
rebeccabr1 <rebeccaabourjeily2@gmail.com>
2024-07-30 21:39:45 +0300

    done2

--- src/task manager/Todomanaging.js
+++ src/task manager/Todomanaging.js
@@ -1,16 +1,14 @@
 
 // todo task for testing
-let todos = [
-    { id: 1, task: 'Do the beds', completed: false },
-    { id: 2, task: 'Take out the dog', completed: true },
-  ];
+
   
   // Get all Todos with error handling
   const getAllTodos = (req, res) => {
     tryCatch((req, res) => {
-    res.json(todos);
+    
     const getquery = 'SELECT * FROM TodoList';
     const gettodos = db.prepare(getquery).all();
+    res.json(gettodos);
     return res.status(200).json(json);
     })
   };
@@ -24,11 +22,11 @@ let todos = [
       return res.status(400).json({ message: 'U must add a task' });
     }
  
-    const newquery = 'INSERT INTO todos (task ) VALUES (?)';
+    const newquery = 'INSERT INTO TodoList (task ) VALUES (?)';
     const newtodos = db.prepare(newquery).run(task);
     const newTodoid = { id: info.lastInsertRowid, task};
-    todos.push(newTodo);
-    res.status(201).json(newTodo);
+
+    res.status(201).json(newTodos);
   });
   };
   
@@ -37,22 +35,24 @@ let todos = [
     const { id } = req.params;
     const { task, completed } = req.body;
   
-    const todo = todos.find(t => t.id === parseInt(id));
+    const updatequery = 'SELECT * FROM TodoList WHERE id = ?';
+
+    const updatetodo= db.prepare(updatequery).get(id);
     if (!todo) return res.status(404).json({ message: 'Todo not found' });
   
     
     const updatedTask = task !== undefined ? task : todo.task;
-    const updatequery='UPDATE todos SET task = ?, WHERE id = ?';
-    const updatetodo= db.prepare(updatequery).run(updatedTask,id);
+    const updatedquery='UPDATE TodoList SET task = ?, WHERE id = ?';
+    const updatedtodo= db.prepare(updatedquery).run(updatedTask,id);
   
-    res.json(todo);
+    res.json(updatedtodo);
   };
   
   // Delete a Todo
   const deleteTodo = (req, res) => {
     const { id } = req.params;
    
-    const deletequery = 'DELETE FROM todos WHERE id = ?';
+    const deletequery = 'DELETE FROM TodoList WHERE id = ?';
 
    const deletetodo= db.prepare(deletequery).run(id);
     res.status(204).send();
